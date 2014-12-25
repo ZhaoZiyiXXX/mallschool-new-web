fca20f835451dec5bafd65808433a598f694f8f3
Ivan Zhao <752251080@qq.com>
2014-12-25 17:35:38 +0800

    debug

--- index.php
+++ index.php
@@ -20,12 +20,24 @@ foreach ($urls as $reg => $tpl) {
         }
         $smarty->assign("api_url", "http://api.mallschool.com");
         //如果连接指向了php文件，说明设计模式有问题，不应该出现php页面
+        if("AlipayReturn.php"==$tpl){
+        	$out_trade_no = $_GET['out_trade_no'];//商户网站唯一订单号
+        	$subject = $_GET['subject'];//商品名称
+        	$trade_status = $_GET['trade_status'];//交易状态
+        	$body = $_GET['body'];//商品描述
+        	$extra_common_param = $_GET['extra_common_param'];//公用回传参数，自定义返回
+        	header("Location: /templates/AlipayReturn.php?out_trade_no=".
+        	$out_trade_no."&subject=".$subject."&trade_status=".$trade_status."&body=".$body."&extra_common_param=".$extra_common_param); 
+			//确保重定向后，后续代码不会被执行 
+			exit;
+        }
         if(".php" == substr($tpl, -4)){
         	$smarty->display($tpl);
         }else{
         	$smarty->display($tpl.'.html');
         }
         //$smarty->display($tpl.'.html');
+        
         break;
     }
 }
--- templates/AlipayReturn.php
+++ templates/AlipayReturn.php
@@ -1,4 +1,39 @@
-{!extends file='base.html'!} {!block name=base!}
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+<title>在线打印</title>
+
+<!-- Bootstrap -->
+<link href="/static/css/bootstrap.min.css" 	rel="stylesheet">
+<link href="/static/css/lib.css" rel="stylesheet">
+<link href="/static/css/main.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+    <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="/static/js/jquery-1.11.1.min.js"></script>
+<script src="/static/js/bootstrap.min.js"></script>
+<script src="/static/js/jquery.easing.1.3.js"></script>
+</head>
+<body>
+	<div class="top">
+	<a class="menu-left" href="/"><span
+				class="glyphicon glyphicon-home"></span> 首 页</a> 
+	<a class="menu-right" href="/query"><span 
+				class="glyphicon glyphicon-question-sign"></span>订单查询 </a>
+	<a class="menu-right" href="/upload"><span
+				class="glyphicon glyphicon-edit"></span> 提交订单</a>
+
+	</div>
+	
 <style>
 .qrcode{
 	width:50%;
@@ -22,13 +57,16 @@
 	$subject = $_GET['subject'];//商品名称
 	$trade_status = $_GET['trade_status'];//交易状态
 	$body = $_GET['body'];//商品描述
-	$extra_common_parm = $_GET['extra_common_parm'];//公用回传参数，自定义返回
+	$extra_common_param = $_GET['extra_common_param'];//公用回传参数，自定义返回
 ?>
 <div class="row tac">
 	<h1>支付成功！</h1>
 </div>
 <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2">
-      	<div class="qrcode"><img src="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=<?php echo urldecode($extra_common_parm)?>"  id="qrcodeimg"/></div>
-      	<div class="content"><p>用微信扫描左侧二维码，关注微信平台并绑定该订单，订单打印完成之后会向您发送通知</p></div>
+      	<div class="qrcode"><img src="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=<?php echo $extra_common_param;?>"  id="qrcodeimg"/></div>
+      	<div class="content"><h2>用微信扫描左侧二维码，关注微信平台并绑定该订单，订单打印完成之后会向您发送通知</h2></div>
 </div>
-{!/block!}
+
+
+</body>
+</html>
